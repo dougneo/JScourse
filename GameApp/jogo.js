f0ef545665c314c20c2befd1b5b3700f6776b61b
Douglas Silva <doug.neo@gmail.com>
2021-01-11 11:15:26 -0300

    Encerrado todas as atividades

--- GameApp/jogo.js
+++ GameApp/jogo.js
@@ -1,5 +1,25 @@
 var altura = 0
 var largura = 0
+var vidas = 1
+var tempo = 15
+
+var criarMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', "")
+
+if (nivel === 'normal') {
+    //1500
+    criarMosquitoTempo = 1500
+} else if (nivel === 'dificil') {
+    //1000
+    criarMosquitoTempo = 1000
+} else if (nivel === 'chucknorris') {
+    //750
+    criarMosquitoTempo = 750
+}
+
+
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
@@ -10,7 +30,36 @@ function ajustaTamanhoPalcoJogo() {
 
 ajustaTamanhoPalcoJogo()
 
+var cronometro = setInterval(function() {
+    document.getElementById('cronometro').innerHTML = tempo
+    tempo -= 1
+
+    if (tempo < 0 && vidas <= 3) {
+        clearInterval(cronometro)
+        clearInterval(criaMosca)
+        window.location.href = 'win.html'
+    } else {
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+
+}, 1000)
+
+
 function posicaoRandomica() {
+
+    //Remover a mosca anterior (caso exista)
+    if (document.getElementById('mosca')) {
+        document.getElementById('mosca').remove()
+
+        if (vidas > 3) {
+
+            window.location.href = 'gameover.html'
+        } else {
+            document.getElementById('v' + vidas).src = 'imagens/coracao_vazio.png'
+            vidas++
+        }
+    }
+
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -26,6 +75,10 @@ function posicaoRandomica() {
     mosca.style.left = posicaoX + 'px'
     mosca.style.top = posicaoY + 'px'
     mosca.style.position = 'absolute'
+    mosca.id = 'mosca'
+    mosca.onclick = function() {
+        this.remove()
+    }
 
 
     document.body.appendChild(mosca)
